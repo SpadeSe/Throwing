457b73e3e95f864f39cba4ef9ece01ae1e98d688
SpadeSe <2921576608@qq.com>
2020-06-26 01:07:17 +0800

    实现道具生成

--- Assets/Scripts/Items/ItemGenerator.cs
+++ Assets/Scripts/Items/ItemGenerator.cs
@@ -6,20 +6,34 @@ using UnityEngine;
 public class ItemGenerator : MonoBehaviour
 {
     public bool CanGen = true;
+    Coroutine genRoutine;
     [Header("Settings")]
     public float startGenDelay = 1.0f;
     public float GenInterval = 5.0f;
-    public float startSpeed = 2.0f;
+    //public float startSpeed = 2.0f;
+    public float itemDuration = 8.0f;
+    public float itemRotateSpeed = 3.0f;
     [Header("Refs")]
     public Transform startTrans;
-    public Transform dirTrans;
+    //public Transform dirTrans;
+    public Transform endTrans;
     public List<GameObject> ItemPrefabs;
     // Start is called before the first frame update
     void Start()
     {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach(var renderer in renderers)
+        {
+            renderer.enabled = false;
+        }
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        foreach(var collider in colliders)
+        {
+            collider.enabled = false;
+        }
         if (CanGen)
         {
-
+            genRoutine = StartCoroutine(GenItem());
         }
     }
 
@@ -33,6 +47,13 @@ public class ItemGenerator : MonoBehaviour
     IEnumerator GenItem()
     {
         yield return new WaitForSeconds(startGenDelay);
-
+        while (CanGen)
+        {
+            GameObject item = Instantiate(ItemPrefabs[Random.Range(0, ItemPrefabs.Count)], startTrans);
+            item.GetComponent<Rigidbody>().AddForce((endTrans.position - startTrans.position) / itemDuration, ForceMode.Impulse);
+            item.GetComponent<Rigidbody>().AddTorque(item.transform.up * itemRotateSpeed, ForceMode.Impulse);
+            Destroy(item, itemDuration);
+            yield return new WaitForSeconds(GenInterval);
+        }
     }
 }
